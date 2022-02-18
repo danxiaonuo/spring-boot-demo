35ca211d6b5110092a9eb977829ae89148f3a4cb
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-02-18 15:29:50 +0800

    更新

--- target/bin/kill.sh
+++ target/bin/kill.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd `dirname $0`
-cd ..
-VAR_DEPLOY_DIR=`pwd`
-VAR_PIDS=`ps aux | grep java | grep "$VAR_DEPLOY_DIR" | grep -v grep | grep -v "kill.sh" | awk '{print $2}'`
+VAR_DEPLOY_DIR="/usr/spring-boot-demo"
+cd $VAR_DEPLOY_DIR
+
+VAR_PIDS=`ps aux | grep java | grep "$VAR_DEPLOY_DIR" | grep -v grep | grep -v "kill.sh" | awk '{print $1}'`
 
 if [ -z "$VAR_PIDS" ]; then
 echo "no process"
--- target/bin/start.sh
+++ target/bin/start.sh
@@ -5,7 +5,7 @@ source /etc/profile
 VAR_DEPLOY_DIR="/usr/spring-boot-demo"
 VAR_LOG_DIR=$VAR_DEPLOY_DIR/logs
 VAR_STDOUT_FILE=$VAR_LOG_DIR/stdout.log
-cd /usr/spring-boot-demo
+cd $VAR_DEPLOY_DIR
 
 VAR_JAVA_OPTS="-Xmx1550m -Xms1550m -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=128M -Xss256K -XX:+UseConcMarkSweepGC -XX:+UseParNewGC"
 # SKYWALKING链路跟踪
@@ -21,5 +21,5 @@ MAIN_CLASS="com.iflytek.iptv.Main"
 # 启动时同步加载conf、resources、lib目录下的资源
 nohup java $VAR_JAVA_OPTS $SKYWALKING_OPTIONS -cp $VAR_DEPLOY_DIR/conf:$VAR_DEPLOY_DIR/resources:$VAR_DEPLOY_DIR/lib/* $MAIN_CLASS > $VAR_STDOUT_FILE > /dev/null 2>&1 &
 
-VAR_PIDS=`ps aux | grep java | grep "$VAR_DEPLOY_DIR" | grep -v grep | grep -v "start.sh" | awk '{print $2}'`
+VAR_PIDS=`ps aux | grep java | grep "$VAR_DEPLOY_DIR" | grep -v grep | grep -v "start.sh" | awk '{print $1}'`
 echo "PID: $VAR_PIDS"
